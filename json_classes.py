6376b347bfebe142fb9c54e2ca529d7c56c85cf5
estelasc <escarballo@hotmail.com>
2019-05-03 00:09:32 +0200

    Some classes important for the advanced level of the Final Project

--- json_classes.py
+++ json_classes.py
@@ -1,43 +1,50 @@
+# Some useful classes for the advanced level of the final project
 import json
 
 
 class UserEncoder(json.JSONEncoder):
-
+    """Class for encode data in json format"""
     def default(self, obj):
         return obj.__dict__
 
 
 class Error(object):
+    """Class for catching errors"""
 
     def __init__(self, ErrorDescription):
         self.ErrorDescription = ErrorDescription
 
 
 class AvailableSpecies(object):
+    """Class for showing all or some of the available species in the Ensembl database"""
 
     def __init__(self, Species):
         self.Species = Species
 
 
 class SpeciesChromosomes(object):
+    """Class for showing the name of all the chromosomes of a given specie"""
 
     def __init__(self, Chromosomes):
         self.Chromosomes = Chromosomes
 
 
 class ChromosomeLength(object):
+    """Class for showing the length of a given chromosome"""
 
     def __init__(self, Length):
         self.Length = Length
 
 
 class GeneSequence(object):
+    """Class for showing the sequence of a given human gene"""
 
     def __init__(self, Sequence):
         self.Sequence = Sequence
 
 
 class Gene_Info(object):
+    """Class for showing some information about a given human gene"""
 
     def __init__(self, Start, End, Length, Id, Chromosome):
         self.Start = Start
@@ -48,6 +55,7 @@ class Gene_Info(object):
 
 
 class Gene_Calculations(object):
+    """Class for showing some calculations on the sequence of a given human gene"""
 
     def __init__(self, LengthA, LengthC, LengthG, LengthT, PercA, PercC, PercG, PercT):
         self.LengthA = LengthA
@@ -61,13 +69,7 @@ class Gene_Calculations(object):
 
 
 class GenesChromosome(object):
+    """Class for showing a list with the genes located in a given region within a given chromosome"""
 
     def __init__(self, Genes):
         self.Genes = Genes
-
-
-class User(object):
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
